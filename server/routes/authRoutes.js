624e90f0f79feb8fbcec70da8ab1e6616bc52d02
MohitSingh25k <mohitsingh.rdr25@gmail.com>
2026-07-22 20:21:27 +0530

    Added authmiddleware and profile route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,8 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const { registerUser, loginUser } = require("../controllers/authControllers");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", authMiddleware, (req, res) => {
+    res.json({
+        success: true,
+        user: req.user,
+    });
+});
 
 module.exports = router;
